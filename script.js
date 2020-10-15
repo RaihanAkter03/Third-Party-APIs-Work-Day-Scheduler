893e69ec27b355cd5bd2e0883a956aee92b731ec
RaihanAkter03 <raihanakter26@gmail.com>
2020-10-15 12:07:21 -0500

    made changes on code and readme

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 
 $(document).ready(function () { 
+    //random flag
     var random = false;
-    // var rowTextArea = $(".textarea");
-    // var saveButton = $(".saveBtn");
     var currentDay = $("#currentDay");
-
     var hour12 = moment().format('h');
     var hour24 = moment().format('H');
-
-    if (random) { 
+    
+    if (random) {
         hour12 = 1;
         hour24 = 13;
-    }
+    } 
+
     function displaurrentDate()
     {
         var displayDate = moment().format("dddd") + " , " + moment().format("MMMM Do");
@@ -22,15 +21,14 @@ $(document).ready(function () {
 
     var dailyPlanArr = JSON.parse(localStorage.getItem("planStore")) || new Array(9);
    
-
-    console.log(dailyPlanArr);
+     console.log(dailyPlanArr);
+    
 
     //main continer 
     var container = $(".container");
-
-    
-    // function Displayplaning() { 
-        for (var hour = 9; hour <= 17; hour++) { 
+    container.empty();
+   
+        for (var hour = 9; hour <= 17; hour++) {
             var index = hour - 9;
             
             //main row ...
@@ -41,11 +39,10 @@ $(document).ready(function () {
 
             //hour column
             var hourColumn = $("<div>");
-            hourColumn.addClass("col-md-1");
+            hourColumn.addClass("col-sm-1");
             hourColumn.addClass("hour")
 
             var timeEl = $("<span>");
-        
 
             var hourDisplay = 0;
             var ampm = "";
@@ -57,6 +54,7 @@ $(document).ready(function () {
                 ampm = "am";
             }
             container.append(rowDiv);
+
             //daily hour..
             timeEl.text(hourDisplay + " " + ampm);
             rowDiv.append(hourColumn);
@@ -69,10 +67,10 @@ $(document).ready(function () {
             dailyPlan.attr("class", "textarea");
 
             var textColumn = $("<div>");
-            textColumn.addClass("col-md-10");
+            textColumn.addClass("col-sm-10");
             textColumn.addClass('description')
 
-            dailyPlan.attr("id","input-"+index);
+            dailyPlan.attr("id", "input-" + index);
             rowDiv.append(textColumn);
             textColumn.append(dailyPlan);
 
@@ -80,33 +78,24 @@ $(document).ready(function () {
 
             //save button 
             var buttonColumn = $("<div>");
-            buttonColumn.addClass("col-md-1");
+            buttonColumn.addClass("col-sm-1");
             buttonColumn.attr("id", "saveBtn");
             buttonColumn.addClass('saveBtn')
 
             var saveButton = $("<i>")
             saveButton.attr("class", "far fa-save saveIcon");
             saveButton.attr("save-index", index);
-
             
             rowDiv.append(buttonColumn);
             buttonColumn.append(saveButton);
-
            
-            console.log(rowDiv + "and hour" + hour);
-            colorEffect(textColumn, hour);
-        }
+            console.log(rowDiv + "and hour " + hour);
+            colorEffect(textColumn, hour); // calling function 
+        }// loop end for displaying data and rows
 
-
-    // }// function ends
-   
-    // Displayplaning();
-
-   function colorEffect(hourRow, hour) { 
-        if (random) { 
-            console.log("rowcolor", hour24 , hour)
-        }
-       if (hour < hour24) {
+    function colorEffect(hourRow, hour) { 
+        console.log(hour);
+       if (hour < hour24 || hour < hour12) {
          hourRow.addClass("past");           
        } else if (hour > hour24) { 
            hourRow.addClass("future");
@@ -116,6 +105,7 @@ $(document).ready(function () {
 
     }
    
+    // save button works ..
     $(document).on('click', 'i', function (event) { 
         event.preventDefault();
         var saveIndex = $(this).attr("save-index");
